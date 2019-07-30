c16f66ecf65edbce4fab283aeb1bb213f54a6021
Ming Sun <ming.sun@sky.uk>
2019-07-30 18:02:59 +0100

    Day4 part1 progress

--- src/main/scala/advent2018/Day4.scala
+++ src/main/scala/advent2018/Day4.scala
@@ -1,5 +1,8 @@
 package advent2018
 import java.time.{LocalDate, LocalDateTime}
+
+import com.sun.net.httpserver.Authenticator.Failure
+
 import scala.io.Source
 
 sealed trait Event { val value: String }
@@ -7,9 +10,9 @@ case object BeginShift extends Event { val value = "begins shift"}
 case object FallSleep extends Event { val value = "falls asleep"}
 case object WakeUp extends Event {val value = "wakes up"}
 
-case class EventLog(date: LocalDateTime, id: Int, event: Event)
+case class EventLog (date: LocalDateTime, id: Int, event: Event)
 
-
+case class Shift (id: Int, sleeps: Array[Int])
 
 object Day4 extends Day4 with App {
 
@@ -24,9 +27,34 @@ object Day4 extends Day4 with App {
 
 
 trait Day4 {
-  def part1(data: Seq[String]): Int = ???
+  def part1(data: Seq[String]): Int = {
+    import cats.Monoid
+    import cats.implicits._
 
-  val pattern1 = "^\\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\\] Guard #([0-9]+) (.+)$".r
+    implicit val shiftMonoid: Monoid[Shift] = new Monoid[Shift] {
+      override def empty: Shift = Shift(0, new Array[Int](60))
+      override def combine(x: Shift, y: Shift): Shift = Shift(x.id, x.sleeps.zip(y.sleeps).map {case (a, b) => a + b})
+    }
+
+    val log: Map[Int, Iterable[Shift]] = data.map(parseLine)
+      .map(fixEventDate)
+      .sortWith {case (a, b) => a.date.compareTo(b.date) < 0 }
+      .groupBy(_.date.toLocalDate)
+      .mapValues(getShift _)
+      .values
+      .groupBy(_.id)
+
+    val shifts: Map[Int, Shift] = log.mapValues(_.combineAll)
+
+    shifts.mapValues(_.sleeps.sum)
+      .max
+      ._1
+  }
+
+//    [1518-05-30 00:04] Guard #2417 begins shift
+//    [1518-10-20 00:48] wakes up
+
+  val pattern1 = """^\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] Guard #(\d{1,4}) (.+)$""".r
   val pattern2 = "^\\[([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)\\] (.+)$".r
   def parseLine(line: String): EventLog = line match {
     case pattern1(y, m, d, h, mm, id, e) => EventLog(LocalDateTime.of(y.toInt, m.toInt, d.toInt, h.toInt, mm.toInt), id.toInt, matchEvent(e))
@@ -39,6 +67,21 @@ trait Day4 {
     case WakeUp.value => WakeUp
   }
 
+
+  def fixEventDate(log: EventLog): EventLog = log match {
+    case EventLog(dt, id, BeginShift) if (dt.getHour == 23) => EventLog(dt.plusDays(1).withHour(0).withMinute(0), id, BeginShift)
+    case _ => log
+  }
+
+  def getShift(log: Seq[EventLog]): Shift = {
+    val array = log.foldLeft(new Array[Int](60))((acc, l) => l.event match {
+      case BeginShift => acc
+      case FallSleep => acc.slice(0, l.date.getMinute) ++ acc.slice(l.date.getMinute, 60).map(_ => 1)
+      case WakeUp => acc.slice(0, l.date.getMinute) ++ acc.slice(l.date.getMinute, 60).map(_ => 0)
+    })
+
+    Shift(log.find(_.event == BeginShift).map(_.id).getOrElse(0), array)
+  }
 }
 
 
--- src/test/scala/advent2018/Day4Spec.scala
+++ src/test/scala/advent2018/Day4Spec.scala
@@ -1,11 +1,15 @@
 package advent2018
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import org.scalatest.{Matchers, WordSpec}
-import Day4.parseLine
+import Day4._
 
 class Day4Spec extends WordSpec with Matchers {
+
+  val datePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+  def parseDate(date: String): LocalDateTime = LocalDateTime.parse(date, datePattern)
 
   "part1 parseLine" should {
     "parse begins shift" in {
@@ -27,6 +31,62 @@ class Day4Spec extends WordSpec with Matchers {
       result.date shouldEqual LocalDateTime.of(1518, 8, 12, 0, 14)
       result.id shouldEqual 0
       result.event shouldEqual FallSleep
+    }
+  }
+
+  "part1 getShift" should {
+    "get shifts for id 10" in {
+      val events = """[1518-11-01 00:00] Guard #10 begins shift
+                |[1518-11-01 00:05] falls asleep
+                |[1518-11-01 00:25] wakes up
+                |[1518-11-01 00:30] falls asleep
+                |[1518-11-01 00:55] wakes up""".stripMargin.split("\n").map(parseLine)
+
+      val expectedArr = ".....####################.....#########################.....".map {
+        case '.' => 0
+        case '#' => 1
+      }.toArray
+
+      val shift = getShift(events)
+      shift.id shouldBe 10
+      shift.sleeps shouldBe expectedArr
+    }
+  }
+
+  "get shifts for id 99" in {
+    val events = """[1518-11-01 23:58] Guard #99 begins shift
+                |[1518-11-02 00:40] falls asleep
+                |[1518-11-02 00:50] wakes up""".stripMargin.split("\n").map(parseLine)
+
+    val expectedArr = "........................................##########..........".map {
+      case '.' => 0
+      case '#' => 1
+    }.toArray
+
+    val shift = getShift(events)
+    shift.id shouldBe 99
+    shift.sleeps shouldBe expectedArr
+  }
+
+
+  "part1 fixDate" should {
+    "return same date when shift start at 00:00" in {
+      val date = parseDate("1518-05-30 00:04")
+      val log = EventLog(date, 10, BeginShift)
+      fixEventDate(log) shouldBe log
+    }
+
+    "return next day at 00:00 when shift start at 23pm" in {
+      val date = parseDate("1518-05-29 23:54")
+      val log = EventLog(date, 10, BeginShift)
+      val expectedLog = EventLog(parseDate("1518-05-30 00:00"), 10, BeginShift)
+      fixEventDate(log) shouldBe expectedLog
+    }
+
+    "return same date when not shift start" in {
+      val date = parseDate("1518-05-30 00:54")
+      val log = EventLog(date, 10, WakeUp)
+      fixEventDate(log) shouldBe log
     }
   }
 }
