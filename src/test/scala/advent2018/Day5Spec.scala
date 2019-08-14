aad2bc2eff4044869f248d3e85f92a5790500be4
Ming Sun <ming.sun@sky.uk>
2019-08-14 13:23:16 +0100

    Day5 improved tests

--- src/test/scala/advent2018/Day5Spec.scala
+++ src/test/scala/advent2018/Day5Spec.scala
@@ -2,6 +2,7 @@ package advent2018
 
 import org.scalatest.{Matchers, WordSpec}
 import Day5Solution._
+import scala.io.Source
 
 class Day5Spec extends WordSpec with Matchers {
 
@@ -21,6 +22,18 @@ class Day5Spec extends WordSpec with Matchers {
       remove("dabAcCaCBAcCcaDA", 'b') shouldBe "daAcCaCAcCcaDA"
       remove("dabAcCaCBAcCcaDA", 'c') shouldBe "dabAaBAaDA"
       remove("dabAcCaCBAcCcaDA", 'd') shouldBe "abAcCaCBAcCcaA"
+    }
+  }
+
+  "Solutions" should {
+    "work for part1" in {
+      val data = Source.fromResource("Day5").getLines().mkString
+      part1(data) shouldBe 11540
+    }
+
+    "work for part2" in {
+      val data = Source.fromResource("Day5").getLines().mkString
+      part2(data) shouldBe 6918
     }
   }
 
